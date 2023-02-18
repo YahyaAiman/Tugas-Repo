02e8a71906fda1ae882821ae279a2679d4b88916
YahyaAiman <yahyaaiman95@gmail.com>
2023-02-19 02:48:26 +0700

    adding IPK

--- test.dart
+++ test.dart
@@ -2,6 +2,7 @@ void main(List<String> args) {
   String nama = 'yahya';
   int nilai = 80;
   double phi = 3.1415926;
+  dynamic IPK = 3.6;
   bool mengulang = false;
 
   for (var i = 0; i <= 10; i++) {
